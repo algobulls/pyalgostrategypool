fa521a8cb936e55936439402ddacb2760cfbd33a
guanidene <guanidene@gmail.com>
2020-05-02 17:27:02 +0530

    Added new strategy - MACD Bracket Order Strategy

---
+++ strategy_macd_bracket_order.py
@@ -0,0 +1,102 @@
+import talib
+
+from pyalgotrading.constants import *
+from pyalgotrading.strategy.strategy_base import StrategyBase
+
+
+class StrategyMACDBracketOrder(StrategyBase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fastMA_period = self.strategy_parameters['fastma_period']
+        self.slowMA_period = self.strategy_parameters['slowma_period']
+        self.signal_period = self.strategy_parameters['signal_period']
+        self.stoploss = self.strategy_parameters['stoploss_trigger']
+        self.target = self.strategy_parameters['target_trigger']
+        self.trailing_stoploss = self.strategy_parameters['trailing_stoploss_trigger']
+
+        self.main_order = None
+
+    def initialize(self):
+        self.main_order = {}
+
+    @staticmethod
+    def name():
+        return 'MACD Bracket Order Strategy'
+
+    @staticmethod
+    def versions_supported():
+        return VERSION_3_1_0
+
+    def get_crossover_value(self, instrument):
+        hist_data = self.get_historical_data(instrument)
+        macdline, macdsignal, _ = talib.MACD(hist_data['close'], fastperiod=self.fastMA_period, slowperiod=self.slowMA_period, signalperiod=self.signal_period)
+        crossover_value = self.utils.crossover(macdline, macdsignal)
+        return crossover_value
+
+    def strategy_select_instruments_for_entry(self, candle, instruments_bucket):
+
+        selected_instruments_bucket = []
+        sideband_info_bucket = []
+
+        for instrument in instruments_bucket:
+            crossover_value = self.get_crossover_value(instrument)
+            if crossover_value == 1:
+                selected_instruments_bucket.append(instrument)
+                sideband_info_bucket.append({'action': 'BUY'})
+            elif crossover_value == -1:
+                if self.strategy_mode is StrategyMode.INTRADAY:
+                    selected_instruments_bucket.append(instrument)
+                    sideband_info_bucket.append({'action': 'SELL'})
+
+        return selected_instruments_bucket, sideband_info_bucket
+
+    def strategy_enter_position(self, candle, instrument, sideband_info):
+        if sideband_info['action'] == 'BUY':
+            qty = self.number_of_lots * instrument.lot_size
+            ltp = self.broker.get_ltp(instrument)
+            self.main_order[instrument] = self.broker.BuyOrderBracket(instrument=instrument,
+                                                                      order_code=BrokerOrderCodeConstants.BROKER_ORDER_CODE_INTRADAY,
+                                                                      order_variety=BrokerOrderVarietyConstants.BROKER_ORDER_VARIETY_LIMIT,
+                                                                      quantity=qty,
+                                                                      price=ltp,
+                                                                      stoploss_trigger=ltp - (ltp * self.stoploss),
+                                                                      target_trigger=ltp + (ltp * self.target),
+                                                                      trailing_stoploss_trigger=ltp * self.trailing_stoploss)
+
+        elif sideband_info['action'] == 'SELL':
+            qty = self.number_of_lots * instrument.lot_size
+            ltp = self.broker.get_ltp(instrument)
+            self.main_order[instrument] = self.broker.SellOrderBracket(instrument=instrument,
+                                                                       order_code=BrokerOrderCodeConstants.BROKER_ORDER_CODE_INTRADAY,
+                                                                       order_variety=BrokerOrderVarietyConstants.BROKER_ORDER_VARIETY_LIMIT,
+                                                                       quantity=qty,
+                                                                       price=ltp,
+                                                                       stoploss_trigger=ltp + (ltp * self.stoploss),
+                                                                       target_trigger=ltp - (ltp * self.target),
+                                                                       trailing_stoploss_trigger=ltp * self.trailing_stoploss)
+        else:
+            raise SystemExit(f'Got invalid sideband_info value: {sideband_info}')
+
+        return self.main_order[instrument]
+
+    def strategy_select_instruments_for_exit(self, candle, instruments_bucket):
+        selected_instruments_bucket = []
+        sideband_info_bucket = []
+
+        for instrument in instruments_bucket:
+            if self.main_order.get(instrument) is not None:
+                crossover_value = self.get_crossover_value(instrument)
+                if crossover_value in [1, -1]:
+                    selected_instruments_bucket.append(instrument)
+                    sideband_info_bucket.append({'action': 'EXIT'})
+        return selected_instruments_bucket, sideband_info_bucket
+
+    def strategy_exit_position(self, candle, instrument, sideband_info):
+        if sideband_info['action'] == 'EXIT':
+            self.main_order[instrument].exit_position()
+            self.main_order[instrument] = None
+            return True
+
+        return False
