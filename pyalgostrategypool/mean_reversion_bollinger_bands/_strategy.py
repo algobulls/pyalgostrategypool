0e09908cbc4e5eb072eb8363c4b591dfcfdf5b20
pratik-urkude-algobulls <pratik.urkude@algobulls.com>
2024-01-13 12:20:11 +0530

    adding golden for every strategy

---
+++ pyalgostrategypool/mean_reversion_bollinger_bands/_strategy.py
@@ -0,0 +1,75 @@
+"""
+    checkout:
+        - strategy specific docs here : https://algobulls.github.io/pyalgotrading/strategies/mean_reversion_bollinger_bands/
+        - generalised docs in detail here : https://algobulls.github.io/pyalgotrading/strategies/strategy_guides/common_strategy_guide/
+"""
+import talib
+from constants import *
+from strategy import StrategyBase
+
+
+class MeanReversionBollingerBands(StrategyBase):
+    name = 'Mean Reversion Bollinger Bands'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.timeperiod = self.strategy_parameters['TIMEPERIOD']
+        self.std_deviation = self.strategy_parameters['STD_DEVIATION']
+        self.main_order_map = None
+
+    def initialize(self):
+        self.main_order_map = {}
+
+    def get_decision(self, instrument):
+        hist_data = self.get_historical_data(instrument)
+        upper_band, _, lower_band = talib.BBANDS(hist_data['close'], timeperiod=self.timeperiod, nbdevup=self.std_deviation, nbdevdn=self.std_deviation, matype=0)
+        upper_band_value = upper_band.iloc[-1]
+        lower_band_value = lower_band.iloc[-1]
+        latest_candle = hist_data.iloc[-1]
+        previous_candle = hist_data.iloc[-2]
+
+        if (previous_candle['open'] <= lower_band_value or previous_candle['low'] <= lower_band_value) and (latest_candle['close'] > previous_candle['close']):
+            action = 'BUY'
+        elif (previous_candle['open'] >= upper_band_value or previous_candle['close'] >= upper_band_value) and (latest_candle['close'] < previous_candle['close']):
+            action = 'SELL'
+        else:
+            action = None
+
+        return action
+
+    def strategy_select_instruments_for_entry(self, candle, instruments_bucket):
+        selected_instruments, meta = [], []
+
+        for instrument in instruments_bucket:
+            if self.main_order_map.get(instrument) is None:
+                action = self.get_decision(instrument)
+                if action is not None:
+                    selected_instruments.append(instrument)
+                    meta.append({'action': action})
+
+        return selected_instruments, meta
+
+    def strategy_enter_position(self, candle, instrument, meta):
+        self.main_order_map[instrument] = _ = self.broker.OrderRegular(instrument, meta['action'], quantity=self.number_of_lots * instrument.lot_size)
+        return _
+
+    def strategy_select_instruments_for_exit(self, candle, instruments_bucket):
+        selected_instruments, meta = [], []
+
+        for instrument in instruments_bucket:
+            if self.main_order_map.get(instrument) is not None:
+                action = self.get_decision(instrument)
+                if action is not None:
+                    selected_instruments.append(instrument)
+                    meta.append({'action': 'EXIT'})
+
+        return selected_instruments, meta
+
+    def strategy_exit_position(self, candle, instrument, meta):
+        if meta['action'] == 'EXIT':
+            self.main_order_map[instrument].exit_position()
+            self.main_order_map[instrument] = None
+            return True
+
+        return False
---
+++ pyalgostrategypool/volatility_trend_atr/_strategy.py
@@ -0,0 +1,77 @@
+"""
+    checkout:
+        - strategy specific docs here : https://algobulls.github.io/pyalgotrading/strategies/volatility_trent_atr/
+        - generalised docs in detail here : https://algobulls.github.io/pyalgotrading/strategies/strategy_guides/common_strategy_guide/
+"""
+import talib
+from constants import *
+from strategy import StrategyBase
+
+
+class VolatilityTrendATR(StrategyBase):
+    name = 'Volatility Trend ATR'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.timeperiod_atr = self.strategy_parameters['TIMEPERIOD_ATR']
+        self.atr_prev_candles_num = self.strategy_parameters['ATR_PREV_CANDLES_NUM']
+        self.main_order_map = None
+        self.previous_trend = None
+        self.current_trend = None
+        self.action_constants = {1: 'BUY', -1: 'SELL'}
+
+    def initialize(self):
+        self.main_order_map = {}
+        self.previous_trend = {}
+        self.current_trend = {}
+
+    def get_trend_direction(self, instrument):
+        hist_data = self.get_historical_data(instrument)
+        atr = talib.ATR(hist_data['high'], hist_data['low'], hist_data['close'], timeperiod=self.timeperiod_atr)
+        current_atr = atr.iloc[-1]
+        atr_prev_candles_num = atr.iloc[-self.atr_prev_candles_num]
+        return 1 if current_atr > atr_prev_candles_num else -1 if current_atr < atr_prev_candles_num else 0
+
+    def strategy_select_instruments_for_entry(self, candle, instruments_bucket):
+        instruments, meta = [], []
+
+        for instrument in instruments_bucket:
+            if self.main_order_map.get(instrument) is None:
+                if self.current_trend.get(instrument) in [None, 0]:
+                    current_trend = self.current_trend[instrument] = self.get_trend_direction(instrument)
+                else:
+                    current_trend = self.current_trend[instrument]
+
+                if current_trend in [1, -1]:
+                    instruments.append(instrument)
+                    meta.append({'action': self.action_constants[current_trend]})
+                    self.previous_trend[instrument] = self.current_trend[instrument]
+
+        return instruments, meta
+
+    def strategy_enter_position(self, candle, instrument, meta):
+        self.main_order_map[instrument] = _ = self.broker.OrderRegular(instrument, meta['action'], quantity=self.number_of_lots * instrument.lot_size)
+        return _
+
+    def strategy_select_instruments_for_exit(self, candle, instruments_bucket):
+        instruments, meta = [], []
+
+        for instrument in instruments_bucket:
+            if self.main_order_map.get(instrument) is not None:
+                current_trend = self.current_trend[instrument] = self.get_trend_direction(instrument)
+                self.current_trend[instrument] = current_trend
+                if current_trend != 0:
+                    if current_trend != self.previous_trend.get(instrument):
+                        instruments.append(instrument)
+                        meta.append({'action': 'EXIT'})
+
+        return instruments, meta
+
+    def strategy_exit_position(self, candle, instrument, meta):
+        if meta['action'] == 'EXIT':
+            self.main_order_map[instrument].exit_position()
+            self.main_order_map[instrument] = None
+            return True
+
+        return False
