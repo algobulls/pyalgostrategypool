3db92b424d8a085ab0d0d22b7af7abcfcedb4e11
pratik-urkude-algobulls <pratik.urkude@algobulls.com>
2024-08-03 22:03:24 +0530

    cleanup

--- pyalgostrategypool/options_bear_call_ladder/_strategy.py
+++ pyalgostrategypool/options_bear_call_ladder/_strategy.py
@@ -4,10 +4,10 @@
         - generalised docs in detail here : https://algobulls.github.io/pyalgotrading/strategies/strategy_guides/common_strategy_guide/
 """
 
-from pyalgotrading.strategy import StrategyOptionsBaseV2, OptionsStrikeDirection, OptionsInstrumentDirection
+from pyalgotrading.strategy import StrategyOptionsBase, OptionsStrikeDirection, OptionsInstrumentDirection
 
 
-class OptionsBearCallLadder(StrategyOptionsBaseV2):
+class OptionsBearCallLadder(StrategyOptionsBase):
     name = 'Options Bear Call Ladder'
 
     def __init__(self, *args, **kwargs):
